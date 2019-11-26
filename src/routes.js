9c5332da2a3250fcd1adf960f402508ff4476c54
andrewdiass <andrew.1995@outlook.com>
2019-11-26 16:59:50 -0300

    homePage create

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter , Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Main from './pages/Main';
+import homePage from './pages/homePage/homePage'
 
 //EXACT = sem o exact ele considera q todas rotas começadas com / serao a login
 export default function Routes() {
-    return(
-        <BrowserRouter>
-        <Route path="/" exact component = {Login} /> 
-        <Route path="/dev/:id" component = {Main} />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Route path="/" component={homePage} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/dev/:id" component={Main} />
+    </BrowserRouter>
+  );
 }
