f821b6c522ce96ff8a4f49337b950a581a64add2
Brennerbbr <brenner.batista@familiapires.com.br>
2019-11-27 08:57:42 -0300

    class

--- src/pages/Quest/Quest.js
+++ src/pages/Quest/Quest.js
@@ -1,105 +1,110 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import "./Quest.css";
-
-/* import api from "../../services/api";
-
-import logo from "../../assets/logo.png"; */
+import api from "../../services/api";
 
 import Menu from "../../components/menu";
 import { Container } from "@material-ui/core";
 
-export default function Login({ history }) {
-  //export exporta assim q ele for renderizado
-  const [exp, setExp] = useState("");
-  const [displayExample, setdisplayExample] = useState("");
+export default class Quest extends Component {
+  state = {
+    exp: "",
+    displayExample: "",
+    quests: "",
+    load: ""
+  };
 
-  /*   async function handleSubmit(e) {
-
-    const response = await api.post("/devs", {
-      //aqu é body
-      username
-    }); //o inicio da rota foi definida dentro do service api
-
-    const { _id } = response.data.dev;
-    history.push(`/dev/${_id}`); //o hisotry é ehrdado do REACT DOM, fazer redirecionamento de paginas é assim
-  } */
-
-  async function func(e) {
+  async componentDidMount() {
+    await this.loadQuest();
+  }
+  loadQuest = async () => {
+    const response = await api.get("/userQuests/all");
+    this.setState({ quests: response.data });
+  };
+  func = async e => {
     e.preventDefault();
     var resp = undefined;
     try {
-      resp = "Resposta: " + eval(exp);
+      resp = "Resposta: " + eval(this.state.exp);
     } catch (e) {
       resp = e;
     }
     document.getElementById("answer").innerHTML = resp;
-  }
-  return (
-    <div>
-      <Menu />
-      <Container
-        style={{ display: displayExample }}
-        id="example"
-        className="example-container"
-      >
-        <div>
-          <h3>
-            <b>Exemplo 1:</b>Qual a área de uma circunferência de raio 2 ?
-          </h3>
-          <form>
-            {/*  <input placeholder="X" onChange={e => setX(e.target.value)}></input>
-            <input placeholder="Y" onChange={e => setY(e.target.value)}></input> */}
-            <textarea
-              style={{ resize: "none" }}
-              type="text"
-              placeholder="Expressao"
-              value={`const raio = 2; \nconst pi = 3.14;\nconst area = 2*pi*Math.pow(raio,2)\narea `}
-              onChange={e => setExp(e.target.value)}
-            ></textarea>
-          </form>
-          <p className="answers" id="ex1">
-            Resposta: 25.12
-          </p>
-          <p id="tutorial1">
-            Nesse exemplo, declaramos 3 variáveis: raio, pi , e area. Onde o
-            raio recebeu o valor do enunciado, pi consideramos um valor
-            aproximado de 3.14 e a área é o resultado esperado na pergunta. Note
-            que para imprimir o resultado final, escrevemos a variável isolada
-            na última linha da entrada. A função
-          </p>
-        </div>
-      </Container>
-      <Container className="example-container">
-        <button
-          id="sendQuest"
-          style={{ textAlign: "center" }}
-          onClick={() =>
-            displayExample !== "none"
-              ? setdisplayExample("none")
-              : setdisplayExample("grid")
-          }
-        >
-          {displayExample === "none" ? "Exibir exemplo" : "Ocultar exemplo"}
-        </button>
-      </Container>
+  };
 
-      <Container className="quest-container">
-        <div>
-          <h3>Pergunta 1 da API?</h3>
-          <form onSubmit={func}>
-            {/*  <input placeholder="X" onChange={e => setX(e.target.value)}></input>
-            <input placeholder="Y" onChange={e => setY(e.target.value)}></input> */}
-            <textarea
-              style={{ resize: "none" }}
-              type="text"
-              placeholder="Escreva o código em javascript:"
-              onChange={e => setExp(e.target.value)}
-            ></textarea>
-            <button type="submit">Enviar</button>
-          </form>
-          <p className="answers" id="answer"></p>
-        </div>
-      </Container>
-    </div>
-  );
+  handleChange(event) {
+    this.setState({ exp: event.target.value });
+  }
+  render() {
+    return (
+      <div>
+        <Menu />
+        <Container
+          style={{
+            display: this.state.displayExample
+              ? this.state.displayExample
+              : "none"
+          }}
+          id="example"
+          className="example-container"
+        >
+          <div>
+            <h3>
+              <b>Exemplo 1:</b>Qual a área de uma circunferência de raio 2 ?
+            </h3>
+            <form>
+              <textarea
+                disabled
+                style={{ resize: "none" }}
+                type="text"
+                placeholder="Expressao"
+                value={`const raio = 2; \nconst pi = 3.14;\nconst area = 2*pi*Math.pow(raio,2)\narea `}
+              ></textarea>
+            </form>
+            <p className="answers" id="ex1">
+              Resposta: 25.12
+            </p>
+            <p id="tutorial1">
+              Nesse exemplo, declaramos 3 variáveis: raio, pi , e area. Onde o
+              raio recebeu o valor do enunciado, pi consideramos um valor
+              aproximado de 3.14 e a área é o resultado esperado na pergunta.
+              Note que para imprimir o resultado final, escrevemos a variável
+              isolada na última linha da entrada. A função
+            </p>
+          </div>
+        </Container>
+        <Container className="example-container">
+          <button
+            id="sendQuest"
+            style={{ textAlign: "center" }}
+            onClick={() =>
+              this.state.displayExample !== "none"
+                ? this.setState({ displayExample: "none" })
+                : this.setState({ displayExample: "grid" })
+            }
+          >
+            {!this.state.displayExample || this.state.displayExample === "none"
+              ? "Exibir exemplo"
+              : "Ocultar exemplo"}
+          </button>
+        </Container>
+
+        <Container className="quest-container">
+          <div>
+            <h3>Pergunta 1 da API?</h3>
+            <form onSubmit={this.func}>
+              <textarea
+                style={{ resize: "none" }}
+                type="text"
+                placeholder="Escreva o código em javascript:"
+                value={this.state.exp}
+                onChange={this.handleChange.bind(this)}
+              ></textarea>
+              <button type="submit">Enviar</button>
+            </form>
+            <p className="answers" id="answer"></p>
+          </div>
+        </Container>
+      </div>
+    );
+  }
 }
