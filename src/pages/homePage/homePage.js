7f4bee2cde6aa42696ad2cf5c9814b98c003a3d9
Andrew Dias <andrew.1995@outlook.com>
2019-11-27 10:38:10 -0300

    Feature(Criação da homePage e nova imagem de background)

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
-import backgroud from '../../assets/backgroud'
+import '../homePage/homePage.css'
+import logo from '../../assets/logo.png'
 
 const homePage = () => (
   <div>
-    <ImageBackground source={backgroud} style={{ width: '100%', height: '100%' }}>
-      <Text>Inside</Text>
-    </ImageBackground>
-    <h1>Teste</h1>
-  </div>
+    <div className="box">
+      <img className="logo" src={logo} alt="logo" />
+      <h2>
+        Saiba como nosso sistema pode ajudar seus alunos a aprenderem
+          programação e ainda estudar matérias escolares como Português,
+          Matemática entre outras...
+      </h2>
+      <h2>
+        Tenha um diferencial das outras escolas, mostre melhorias de
+        resultados e crie pessoal prontas para o futuro…<br />
+        Se destaquem entre as escolas, mostre resultados aos pais,
+        tenham dados de alunos e controle sobre o que eles fazem
+      </h2>
+      <div className="button">
+        <Button variant="contained" color="primary">
+          Conheça a plataforma
+      </Button>
+      </div>
+    </div>
+  </div >
 )
 
 export default homePage;
