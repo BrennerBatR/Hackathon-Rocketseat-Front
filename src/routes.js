0c15ecf6a5eeb280930cdfd686c6bce68148d4d0
Marcelo Alves <marcelo.alves@clubpetro.com.br>
2019-11-27 12:28:09 -0300

    Login e logout

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
-import Main from './pages/Main/Main';
-import Login from './pages/Login';
+import Login from './pages/Login/';
 import StudentRegister from './pages/StudentRegister/StudentRegister';
-import QuestionRegister from './pages/QuestionRegister';
+import QuestionRegister from './pages/QuestionRegister/';
 import Quest from './pages/Quest/Quest';
 import ModuleSelect from './pages/ModuleSelect';
+import HomePage from './pages/homePage/homePage';
 
 export default function Routes() {
     return (
         <BrowserRouter>
-            <Route path="/" exact component={Main}/>
+            <Route path="/" exact component={HomePage}/>
             <Route path="/login" component={Login}/>
             <Route path="/studentsRegister" component={StudentRegister}/>
-            <Route path="/questions/new" component={QuestionRegister}/>
+            <Route path="/questionsRegister" component={QuestionRegister}/>
             <Route path="/quest" component = {Quest} />
             <Route path="/moduleSelect" component = {ModuleSelect} />
         </BrowserRouter>
